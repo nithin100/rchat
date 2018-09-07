85a137f8432fe8638c53eb6ba2a55bc2a02a891c
nithinganji <nithinreddy.ganji@rsrit.com>
2018-09-07 11:52:43 -0400

    added cors for sap cf

--- src/main/java/com/rsrit/rchat/security/config/SimpleCorsFilter.java
+++ src/main/java/com/rsrit/rchat/security/config/SimpleCorsFilter.java
@@ -27,7 +27,7 @@ public class SimpleCorsFilter implements Filter {
 			throws IOException, ServletException {
 		HttpServletResponse response = (HttpServletResponse) res;
 		HttpServletRequest request = (HttpServletRequest) req;
-		response.setHeader("Access-Control-Allow-Origin", "http://localhost:4200");
+		response.setHeader("Access-Control-Allow-Origin", "*");
 		response.setHeader("Access-Control-Allow-Methods", "*");
 		response.setHeader("Access-Control-Max-Age", "3600");
 		response.setHeader("Access-Control-Allow-Credentials", "true");
